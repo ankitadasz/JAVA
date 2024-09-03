e9a94c6131b4e68200909ab97c3e89433ea8d276
Ankita Das <daszankita23@gmail.com>
2024-09-03 20:19:21 +0530

    changes done

---
+++ sumoftwono.java
@@ -0,0 +1,10 @@
+public class sumoftwono {
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 5;
+        int sum = a+b;
+        System.out.print("sum is ");
+        System.out.println(sum);
+
+    }
+}
