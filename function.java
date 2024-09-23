3d05e6340ac6d346aea299d4e872c0ebb472ddc3
Ankita Das <daszankita23@gmail.com>
2024-09-23 21:36:28 +0530

    changes done

--- function.java
+++ function.java
@@ -1,12 +1,21 @@
+import java.util.*;
 public class function {
+ 
     public static void printhelloworld(){
         System.out.println("Hello Ankita");
         System.out.println("Hello Ankita");
         System.out.println("Hello Ankita");
-
-
+    }
+    public static void sumoftwo(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a and b:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println("sum of this two number is:"+c);
     }
     public static void main(String[] args) {
         printhelloworld();
+        sumoftwo();
     }
 }
