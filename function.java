099218963d99d205ceb59731ef59ae273b070bc9
Ankita Das <daszankita23@gmail.com>
2024-09-23 21:29:32 +0530

    changes done

---
+++ function.java
@@ -0,0 +1,12 @@
+public class function {
+    public static void printhelloworld(){
+        System.out.println("Hello Ankita");
+        System.out.println("Hello Ankita");
+        System.out.println("Hello Ankita");
+
+
+    }
+    public static void main(String[] args) {
+        printhelloworld();
+    }
+}
